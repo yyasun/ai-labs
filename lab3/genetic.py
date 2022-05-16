2195deff5a4303195ecc4e186ea232ee6f0c30ef
Yaroslav Pererva <pablo1231435@gmail.com>
2022-05-16 16:22:31 +0300

    split into sepparate files

---
+++ lab3/genetic.py
@@ -0,0 +1,86 @@
+from audioop import avg
+from cmath import sqrt
+import matplotlib.pyplot as plt
+import numpy as np
+
+import random as rnd
+from ast import literal_eval
+import collections
+
+class indiv(list):
+    def __init__(self, genes_v, fit_vlv):
+        self.fit = fit_vlv
+        self.extend(genes_v)
+    
+    def __str__(self):
+        return f"{self[:]}, fit: {self.fit}"
+    
+    def __eq__(self, r):
+        flag = True
+        for i in range(len(r)):
+            flag = flag and self[i] == r[i]
+            if not flag:
+                return False
+        return flag
+
+    def clone(self):
+        return indiv(self[:], self.fit)
+
+
+
+def generate(count, bounds, fit):
+    gen = []
+    for i in range(count):
+        genes = list()
+        for bound in bounds:
+            genes.append(rnd.uniform(bound[0],bound[1]))
+        gen.append(indiv(genes, fit(genes)))
+    return gen
+
+def crossover(m: indiv, f: indiv, fit):
+    child1 = m.clone()
+    child2 = f.clone()
+    c_pnt = rnd.randint(1,len(f)-1)    
+    child1[c_pnt:], child2[c_pnt:] = child2[c_pnt:], child1[c_pnt:]
+    child1.fit = fit(child1[:])
+    child2.fit = fit(child2[:])
+    return [child1, child2]
+
+def mutate(subject: indiv, fit, bounds, mutation_chance):
+    for gene_i in range(len(subject)):
+        if rnd.uniform(0,1) < mutation_chance:
+            subject[gene_i] = np.clip(subject[gene_i] + rnd.uniform(-.5,.5),bounds[gene_i][0],bounds[gene_i][1])
+    subject.fit = fit(subject[:])
+
+def selection(gen: list):
+    avg_fit = sum([g.fit for g in gen])/len(gen)
+    breaders = [indiv for indiv in gen if indiv.fit <= avg_fit]
+    return breaders
+
+def GA(gen_len: int, mutation_chance: float, bounds: list, objective):
+    mutation_chance = mutation_chance
+    gen = generate(gen_len, bounds, objective)
+    best = 100000000
+
+    for i in range(1000):
+        avg_fit = sum([g.fit for g in gen])/len(gen)
+        gen_best = min([g.fit for g in gen]) 
+        if best > gen_best:
+            best = gen_best
+        print(f"{i}. gen best fit: {gen_best}  |   avg fit: {avg_fit}\n")
+    
+        breaders = selection(gen)
+        if not breaders or len(breaders) == 1:
+            breaders = gen
+        new_gen = []
+        for j in range(int(len(gen)/2)):
+            mother = rnd.choice(breaders)
+            father = rnd.choice(breaders)
+            while mother == father: # select two different parents :)
+                mother = rnd.choice(breaders)
+                father = rnd.choice(breaders)    
+            children = crossover(mother, father, objective)
+            [mutate(child, objective, bounds, mutation_chance) for child in children]
+            new_gen.extend(children)
+        gen = new_gen
+    return best
--- lab3/main.py
+++ lab3/main.py
@@ -1,103 +1,19 @@
 # 13, 2, 12
-from audioop import avg
-from cmath import sqrt
-import matplotlib.pyplot as plt
-import numpy as np
+import genetic 
 import math
-import random as rnd
-from ast import literal_eval
-import collections
-
-class indiv(list):
-    def __init__(self, genes_v, fit_vlv):
-        self.fit = fit_vlv
-        self.extend(genes_v)
-    
-    def __str__(self):
-        return f"{self[:]}, fit: {self.fit}"
-    
-    def __eq__(self, r):
-        flag = True
-        for i in range(len(r)):
-            flag = flag and self[i] == r[i]
-            if not flag:
-                return False
-        return flag
-
-    def clone(self):
-        return indiv(self[:], self.fit)
 
 def ecly(v: list):
     x, y = v
-    res = -20 * math.exp(-0.2 * sqrt(0.5*(x**2 + y**2))) - math.exp(0.5*(math.cos(2*math.pi*x) + math.cos(2*math.pi*y))) + math.e + 20
+    res = -20 * math.exp(-0.2 * math.sqrt(0.5*(x**2 + y**2))) - math.exp(0.5*(math.cos(2*math.pi*x) + math.cos(2*math.pi*y))) + math.e + 20
     return res
 
 def camel(v: list):
     x, y = v
     return 2*x**2 - 1.05*x**4 + (x**6)/6 + x*y + y**2
 
-def generate(count, bounds, fit):
-    gen = []
-    for i in range(count):
-        genes = list()
-        for bound in bounds:
-            genes.append(rnd.uniform(bound[0],bound[1]))
-        gen.append(indiv(genes, fit(genes)))
-    return gen
-
-def crossover(m: indiv, f: indiv, fit):
-    child1 = m.clone()
-    child2 = f.clone()
-    c_pnt = rnd.randint(1,len(f)-1)    
-    child1[c_pnt:], child2[c_pnt:] = child2[c_pnt:], child1[c_pnt:]
-    child1.fit = fit(child1[:])
-    child2.fit = fit(child2[:])
-    return [child1, child2]
-
-def mutate(subject: indiv, fit, bounds):
-    for gene_i in range(len(subject)):
-        if rnd.uniform(0,1) < mutation_chance:
-            subject[gene_i] = np.clip(subject[gene_i] + rnd.uniform(-.5,.5),bounds[gene_i][0],bounds[gene_i][1])
-    subject.fit = fit(subject[:])
-
-def selection(gen: list):
-    avg_fit = sum([g.fit for g in gen])/len(gen)
-    breaders = [indiv for indiv in gen if indiv.fit <= avg_fit]
-    return breaders
-
-
-def GA(gen_len: int, mutation_chance: float, bounds: list, objective):
-    mutation_chance = mutation_chance
-    gen = generate(gen_len, bounds, objective)
-    best = 100000000
-
-    for i in range(1000):
-        avg_fit = sum([g.fit for g in gen])/len(gen)
-        gen_best = min([g.fit for g in gen]) 
-        if best > gen_best:
-            best = gen_best
-        print(f"{i}. gen best fit: {gen_best}  |   avg fit: {avg_fit}\n")
-    
-        breaders = selection(gen)
-        if not breaders or len(breaders) == 1:
-            breaders = gen
-        new_gen = []
-        for j in range(int(len(gen)/2)):
-            mother = rnd.choice(breaders)
-            father = rnd.choice(breaders)
-            while mother == father: # select two different parents :)
-                mother = rnd.choice(breaders)
-                father = rnd.choice(breaders)    
-            children = crossover(mother, father, objective)
-            [mutate(child, objective, bounds) for child in children]
-            new_gen.extend(children)
-        gen = new_gen
-    return best
-
-
 mutation_chance = 0.1
 bounds = [[-4,4],[-4,4]]
-best = GA(100,mutation_chance, bounds, camel)
+best = genetic.GA(100,mutation_chance, bounds, camel)
 
 print(f"best: {best}")
 # print(f"best fit: {min([g.fit for g in gen])}")
